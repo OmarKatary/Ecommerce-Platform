20d315990ff63b1b21437ffe9e217fad9e327a72
OmarKatary <omar.katary@hotmail.com>
2020-08-21 22:52:08 +0200

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import React from 'react';
 import './App.css';
-import data from './data'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 function App() {
     
   return (
-    <div className="grid-container">
-          <header className="header">
-              <div className="brand">
-                  <a href="index.html">Eccomerce-Brand</a>
-              </div>
-              <div className="header-buttons">
-                  <a href="cart.html">Cart</a>
-                  <a href="signin.html">Sign in</a>
-              </div> 
-          </header>
-          <main className="main">
-              <div className="container">
-                  <ul className="products">
-                      {data.products.map( product => 
-                        <li>
-                          <div className="product">
-                              <img className="product-image" src={product.image} alt="product"/>
-                              <div className="product-name">{product.name}</div>
-                              <div className="product-brand">{product.brand}</div>
-                              <div className="product-price">${product.price}</div>
-                              <div className="product-rating">{product.rating}({product.numberOfReviews})</div>
-                          </div>
-                      </li>    
-                      )}           
-                  </ul>
-              </div>
-          </main>
-          <footer className="footer">
-              Footer
-          </footer>
-    </div>
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="header">
+                    <div className="brand">
+                        <Link href="index.html" to="/" >Eccomerce-Brand</Link>
+                    </div>
+                    <div className="header-buttons">
+                        <a href="cart.html">Cart</a>
+                        <a href="signin.html">Sign in</a>
+                    </div> 
+                </header>
+                <main className="main">
+                    <div className="container">
+                        <Route path="/product/:id" component={ProductPage} />
+                        <Route path="/" exact={true} component={HomePage} />
+
+                    </div>
+                </main>
+                <footer className="footer">
+                    Footer
+                </footer>
+            </div>
+        </BrowserRouter>
   );
 }
 
